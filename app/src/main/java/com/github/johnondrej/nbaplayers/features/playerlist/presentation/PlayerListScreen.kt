a4a175353e86696cedf77d6e4d962d11b6da8573
Ondřej John <ondrej.john@gmail.com>
2023-02-24 12:45:38 +0100

    ✨ Add loading & error placeholders

--- app/src/main/java/com/github/johnondrej/nbaplayers/features/playerlist/presentation/PlayerListScreen.kt
+++ app/src/main/java/com/github/johnondrej/nbaplayers/features/playerlist/presentation/PlayerListScreen.kt
@@ -1,11 +1,14 @@
 package com.github.johnondrej.nbaplayers.features.playerlist.presentation
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -13,10 +16,14 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
@@ -62,6 +69,27 @@ private fun PlayersList(
                 )
             }
         }
+
+        if (players.loadState.isLoading) {
+            item {
+                LoadingItem(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                )
+            }
+        }
+
+        if (players.loadState.isError) {
+            item {
+                ErrorItem(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { players.retry() }
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                )
+            }
+        }
     }
 }
 
@@ -88,3 +116,29 @@ private fun PlayerItem(
         )
     }
 }
+
+@Composable
+private fun LoadingItem(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .size(24.dp)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+private fun ErrorItem(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(id = R.string.player_list_error_retry),
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
+
+private val CombinedLoadStates.isLoading: Boolean
+    get() = append == LoadState.Loading || refresh == LoadState.Loading
+
+private val CombinedLoadStates.isError: Boolean
+    get() = append is LoadState.Error || refresh is LoadState.Error
